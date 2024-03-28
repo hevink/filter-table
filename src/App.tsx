407fecef96f8fc54ef25ffbf16a14a6466bb6af1
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-28 12:37:30 +0530

    Update tsconfig.json and add data.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Input, Switch, Table } from "antd";
-
-const data = [
-  {
-    id: 1,
-    mall: "V R mall",
-    address: "Surat",
-    rating: "A",
-  },
-  {
-    id: 2,
-    mall: "Rahul Raj Mall",
-    address: "dallas",
-    rating: "B",
-  },
-  {
-    id: 3,
-    mall: "Raj Imperial",
-    address: "san francisco",
-    rating: "B",
-    category: "one",
-  },
-  {
-    id: 4,
-    mall: "jane",
-    address: "denver",
-    category: "two",
-    rating: "C",
-  },
-];
-
-// const data = [
-//   {
-//     id: 1,
-//     name: "foo",
-//     city: "dallas",
-//     category: "one",
-//     type: "A",
-//     active: "FALSE",
-//   },
-//   {
-//     id: 2,
-//     name: "bar",
-//     city: "dallas",
-//     category: "one",
-//     type: "B",
-//     active: "FALSE",
-//   },
-//   {
-//     id: 3,
-//     name: "jim",
-//     city: "san francisco",
-//     category: "one",
-//     type: "B",
-//     active: "TRUE",
-//   },
-//   {
-//     id: 4,
-//     name: "jane",
-//     city: "denver",
-//     category: "two",
-//     type: "C",
-//     active: "FALSE",
-//   },
-// ];
+import { data } from "./data";
 
 function App() {
   const initialSelectedFilters: { [key: string]: string[] } = {};
@@ -109,8 +46,11 @@ function App() {
         }) &&
         // Check if the name matches the search name (if provided)
         (searchName.trim() === "" ||
-          (item.name &&
-            item.name.toLowerCase().includes(searchName.trim().toLowerCase())))
+          (item.hasOwnProperty("name") &&
+            (item as { name?: string }).name &&
+            (item as unknown as { name: string }).name
+              .toLowerCase()
+              .includes(searchName.trim().toLowerCase())))
       );
     });
   };
